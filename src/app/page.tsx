bc784d1f77af502d17a959cba7edc50e1ca66d5e
Ankur Rajbongshi <dekalasit@gmail.com>
2025-07-03 22:54:00 +0530

    5th commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
-import { cookies } from 'next/headers'
 import React from 'react'
 
 export default async function page() {
-  const cookieStore = await cookies()
   return (
     <div>page</div>
   )
